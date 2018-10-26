29a1d61e117adb8ae8cf2d9eaf3dd58f191582c3
Hamon Maxime <hmaxime@localhost.localdomain>
2018-10-26 22:57:58 +0200

    changes

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-const {Pool} = require('pg');
+const pg = require('pg');
 const dataBaseUrl = process.env.DATABASE_URL;
 
 const db = {}
@@ -6,16 +6,16 @@ const db = {}
 async function runQuery(query) {
     console.log("dataBaseUrl : " + dataBaseUrl)
     let response = null;
-    const pool = new Pool({
+    const pool = new pg.Pool({
         connectionString:dataBaseUrl,
         ssl:true
     });
-    console.log("Client : " + client);
+    console.log("Client : " + pool);
     try {
         const client= await pool.connect()
         if (client) {
             await client.query(query, function (err, res) {
-                console.log(res);
+                console.log("réponse du serveur : "+res);
                 response = res.rows;
                 client.end()
             })
