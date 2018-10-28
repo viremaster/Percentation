14aa153230c5a3ac95e5f67303e23cf7f889122a
Hamon Maxime <mag.hamon22@gmail.com>
2018-10-28 14:16:04 +0100

    changes

--- js/users.js
+++ js/users.js
@@ -21,7 +21,8 @@ router.post("/app/user", function (req, res) {
 
     let query = `INSERT INTO "public"."users" ("username", "userid", "email", "role", "password") VALUES('${username}', DEFAULT, '${email}', '${userRole}', '${passwordHash}')RETURNING "username", "userid", "email", "role", "password"`;
     (function () {
-        let code = (result=db.insert(query) ? 200 : 500;
+        let code = (result=db.insert(query)) ? 200 : 500;
+        console.log(result);
         res.status(code).end();
     })
 
