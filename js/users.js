c5de3a07bbac393f610465a7f2865302a0105534
Hamon Maxime <mag.hamon22@gmail.com>
2018-10-28 14:18:21 +0100

    changes

--- js/users.js
+++ js/users.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const db = require("./db.js");
 
-router.get("/app/users", function (req, res) {
+router.get("/app/users", async function (req, res) {
     let query = `SELECT * FROM "public"."users"`;
-    let users = db.select(query);
-    console.log("Users : " + users);
+    let users = await db.select(query);
+    console.log("Users : "+users);
     if (users) {
         res.status(200).json(JSON.parse(users));
     } else {
@@ -13,19 +13,17 @@ router.get("/app/users", function (req, res) {
     }
     res.end();
 });
-router.post("/app/user", function (req, res) {
+router.post("/app/user", async function (req, res) {
     let email = req.body.email;
     let username = req.body.name;
     let passwordHash = req.body.password;
     let userRole = 1;
 
     let query = `INSERT INTO "public"."users" ("username", "userid", "email", "role", "password") VALUES('${username}', DEFAULT, '${email}', '${userRole}', '${passwordHash}')RETURNING "username", "userid", "email", "role", "password"`;
-    (function () {
-        let code = (result=db.insert(query)) ? 200 : 500;
-        console.log(result);
-        res.status(code).end();
-    })
 
+    let code = await db.insert(query) ? 200 : 500;
+    console.log("resultat : "+result);
+    res.status(code).end();
 });
 router.get("/app/user/:username", function (req, res) {
     console.log("pas la");
