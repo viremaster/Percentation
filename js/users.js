90cc9f69cc3c3598517f27c480ea0d21dec73406
Hamon Maxime <mag.hamon22@gmail.com>
2018-10-28 14:11:37 +0100

    changes

--- js/users.js
+++ js/users.js
@@ -21,7 +21,7 @@ router.post("/app/user", function (req, res) {
 
     let query = `INSERT INTO "public"."users" ("username", "userid", "email", "role", "password") VALUES('${username}', DEFAULT, '${email}', '${userRole}', '${passwordHash}')RETURNING "username", "userid", "email", "role", "password"`;
     (async function () {
-        let code = (result = await db.insert(query)) ? 200 : 500;
+        let code = await db.insert(query) ? 200 : 500;
         console.log("resultat : " + result);
         res.status(code).end();
     })
