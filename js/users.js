f5fa6668a7fb208fec0c17791965640c90945cb8
Hamon Maxime <mag.hamon22@gmail.com>
2018-10-30 09:00:35 +0100

    changes

--- js/users.js
+++ js/users.js
@@ -4,7 +4,7 @@ const db = require("./db.js");
 
 router.get("/app/users", function (req, res) {
     let query = `SELECT * FROM public.users`;
-    let users = Promise.resolve(db.select(query));
+    let users = db.select(query)
     console.log("Users : "+users);
     if (users) {
         res.status(200).json(JSON.parse(users));
