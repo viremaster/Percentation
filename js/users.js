55624ad31cbd15da3ba8da54b0d71fc76076b1a4
Hamon Maxime <mag.hamon22@gmail.com>
2018-11-19 17:05:46 +0100

    Add tokens and update for accounts

--- js/users.js
+++ js/users.js
@@ -7,7 +7,6 @@ const saltRounds = 10;
 
 const TOKEN_KEY = process.env.TOKEN_KEY;
 
-
 /*
 router.get("/app/users", async function (req, res) {
     let query = `SELECT * FROM public.users`;
@@ -54,7 +53,9 @@ router.get("/app/user", async function (req, res) {
                 let token = jwt.sign({
                     id: user.userid,
                     username: user.username
-                }, TOKEN_KEY,{expiresIn:86400});
+                }, TOKEN_KEY, {
+                    expiresIn: 86400
+                });
 
 
                 res.status(200).send({
@@ -74,39 +75,58 @@ router.get("/app/user", async function (req, res) {
     } else {
         //Do something
     }
-})
+});
 router.delete("/app/user", async function (req, res) {
-    let auth = req.headers.authorization;
-    if (auth) {
+    let token = req.headers['x-access-token'];
+    let buffToken = new Buffer(token, 'base64');
+    let stringToken = buffToken.toString();
+    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
+    if (decodedToken) {
+        console.log(decodedToken);
+        let query = `DELETE FROM "public"."users" WHERE userid='${decodedToken.id}' RETURNING "username", "email"`;
+        response = await db.delete(query);
+        res.status(200).json(response);
+    } else {
+        res.status(400).send("Wrong token");
+    }
+    res.end();
+});
+router.put("/app/user", async function (req, res) {
+    console.log("yes!");
+    let token = req.headers['x-access-token'];
+    let buffToken = new Buffer(token, 'base64');
+    let stringToken = buffToken.toString();
+    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
+    if (decodedToken) {
+        let auth = req.headers.authorization;
         let tmp = auth.split(' ');
         let buf = new Buffer(tmp[1], 'base64');
         let stringAuth = buf.toString();
         let credentials = stringAuth.split(':');
-        let email = credentials[0];
-        let password = credentials[1];
-        let response;
+        let oldPassword = credentials[0].trim();
+        let newPassword = credentials[1].trim();
 
-        let queryHash = `SELECT password FROM public.users WHERE email='${email}'`;
+        let query = `SELECT password FROM public.users WHERE userid='${decodedToken.id}'`;
 
-        let user = await db.select(queryHash);
-        if (user) {
-            let correctPassword = await bcrypt.compare(password, user.password);
+        let hashPassword = await db.select(query);
+
+        if (hashPassword) {
+            let correctPassword = await bcrypt.compare(oldPassword, hashPassword.password);
+
             if (correctPassword) {
-                let query = `DELETE FROM "public"."users" WHERE email='${email}' RETURNING "username", "email"`;
-
-                response = await db.delete(query);
-
-                res.status(200).json(response);
-            }else{
-                res.status(400).send("Wrong email or password");
+                await bcrypt.hash(newPassword, saltRounds, async function (err, hash) {
+                    query = `UPDATE public.users SET password='${hash}' WHERE userid='${decodedToken.id}' RETURNING "userid"`;
+                    let code = await db.update(query) ? 200 : 500;
+                    res.status(code).send("Password changed").end();
+                })
+            } else {
+                res.status(400).send("Wrong password").end();
             }
-
-        }else{
-            res.status(400).send("Wrong email or password");
+        } else {
+            res.status(500).send("Problème with the server").end();;
         }
-        res.end();
     } else {
-        //Do something
+        res.status(400).send("Wrong token").end();
     }
 })
 
