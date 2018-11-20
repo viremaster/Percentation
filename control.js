8cf9510352a8fe3339cf048eae0de558de6fc788
HMaxime <42681363+HMaxime@users.noreply.github.com>
2018-11-20 13:50:03 +0100

    Integration of functionality
    Add pretty all the text editor functionality, the slides control functionality.

---
+++ control.js
@@ -0,0 +1,72 @@
+let slides = document.getElementsByClassName("currentTemplate");
+let Presentation = document.getElementById("currentSlide");
+let actualSlideIndex = 0;
+let actualSlide = slides[actualSlideIndex];
+
+window.onkeypress = function (e) {
+    if (e.keyCode == 112 || e.keyCode == 37) {
+        previousSlide();
+    } else if (e.keyCode == 110 || e.keyCode == 32 || e.keyCode == 39) {
+        nextSlide();
+    }
+}
+
+function nextSlide() {
+    slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].style.display == "block" && i != slides.length - 1) {
+            slides[i].style.display = "none";
+            slides[i + 1].style.display = "block";
+            actualSlide = slides[i + 1];
+            actualSlideIndex += 1;
+            break
+        }
+    }
+}
+
+function previousSlide() {
+    slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].style.display == "block" && i != 0) {
+            slides[i].style.display = "none";
+            slides[i - 1].style.display = "block";
+            actualSlide = slides[i - 1];
+            actualSlideIndex -= 1
+            break
+        }
+    }
+}
+
+function addSlide() {
+    let newSlide = document.createElement("div");
+    newSlide.className = "slide";
+    newSlide.style.display = "none";
+    if (Presentation.children.length > 0) {
+        Presentation.insertBefore(newSlide, Presentation.children[actualSlideIndex + 1]);
+        nextSlide();
+    } else {
+        Presentation.appendChild(newSlide);
+        newSlide.style.display = "block"
+    }
+    newSlide.innerHTML = `
+    <h1 contenteditable="true"> Insert title here:</h1>
+    <h3 contenteditable="true"> This is a new slide !:</h3>
+    `;
+}
+
+function deleteSlide() {
+    slides = document.getElementsByClassName("slide");
+    if (slides.length > 0) {
+        if (slides.length == 1) {
+            Presentation.removeChild(Presentation.children[actualSlideIndex]);
+            actualSlideIndex = 0;
+        } else if (actualSlideIndex == slides.length - 1) {
+            Presentation.removeChild(Presentation.children[actualSlideIndex]);
+            slides[actualSlideIndex - 1].style.display = "block"
+            actualSlideIndex -= 1;
+        } else {
+            Presentation.removeChild(Presentation.children[actualSlideIndex]);
+            slides[actualSlideIndex].style.display = "block";
+        }
+    }
+}
---
+++ textEditor.js
@@ -0,0 +1,31 @@
+//We defined the variable 
+let buttons = document.getElementById("textToolbar");
+let text = document.getElementById('titleTemplate');
+let fonts = document.getElementById("fontSelect");
+let changeFontButton = document.getElementById("fontName");
+let changeSizeInput = document.getElementById("changeSizeInput");
+
+fonts.onchange = function () {
+    changeFontButton.value = fonts.value;
+    changeFontButton.click();
+}
+
+changeSizeInput.onchange = function () {
+    let fontSize=changeSizeInput.value+"%";
+    let selection=window.getSelection().anchorNode.parentElement;
+    selection.style.fontSize=fontSize;
+}
+
+buttons.onclick = function (evt) {
+    if (evt.target.id != "fontSelect" && evt.target.id != "changeSizeInput") {
+        console.log(evt.target.id);
+        if (evt.target != evt.currentTarget) {
+            if (evt.target.value != "") {
+                value = evt.target.value;
+            } else {
+                value = null;
+            }
+            document.execCommand(evt.target.id, null, value);
+        }
+    }
+}
