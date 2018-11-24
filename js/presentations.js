e8d58b4acb1b2f749951b710ed3ed5e6c2211860
Hamon Maxime <mag.hamon22@gmail.com>
2018-11-24 13:09:16 +0100

    Fix bugs and clean the code

--- js/presentations.js
+++ js/presentations.js
@@ -3,63 +3,37 @@ const router = express.Router();
 const db = require("./db.js");
 const TOKEN_KEY = process.env.TOKEN_KEY;
 const jwt = require('jsonwebtoken');
+const authenticated = require("./authentication.js");
 
 
-router.get("/app/presentations", async function (req, res) {
-    let token = req.headers['x-access-token'];
-    let buffToken = new Buffer(token, 'base64');
-    let stringToken = buffToken.toString();
-    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
-    if (decodedToken) {
-        let query = `SELECT presentationid FROM public.presentations WHERE presentationownerid=${decodedToken.id}`;
-        response = await db.select(query);
-        if (response) {
-            res.status(200).send(response);
-        } else {
-            res.status(200).send("You don't have any presentations")
-        }
+router.get("/app/presentations", authenticated.valideAuthentication, async function (req, res) {
+    let query = `SELECT presentationid FROM public.presentations WHERE presentationownerid=${req.token.id}`;
+    response = await db.select(query);
+    if (response) {
+        res.status(200).send(response);
     } else {
-        res.status(400).send("Wrong token");
+        res.status(200).send("You don't have any presentations")
     }
     res.end;
 
 });
 
-router.get("/app/presentation/:id", async function (req, res) {
-
+router.get("/app/presentation/:id", authenticated.valideAuthentication, async function (req, res) {
     let presentationid = req.params.id;
-    let token = req.headers['x-access-token'];
-    let buffToken = new Buffer(token, 'base64');
-    let stringToken = buffToken.toString();
-    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
-    if (decodedToken) {
-        let query = `SELECT data FROM public.presentations WHERE presentationownerid=${decodedToken.id} AND presentationid=${presentationid}`;
-        response = await db.select(query);
-        response=response[0];
-        res.status(200).send(response);
-    } else {
-        res.status(400).send("Wrong token");
-    }
+    let query = `SELECT data FROM public.presentations WHERE presentationownerid=${req.token.id} AND presentationid=${presentationid}`;
+    response = await db.select(query);
+    response = response[0];
+    res.status(200).send(response);
     res.end();
 
 })
 
-router.post("/app/presentation", async function (req, res) {
+router.post("/app/presentation", authenticated.valideAuthentication, async function (req, res) {
     let data = req.body.data;
-
-    let token = req.headers['x-access-token'];
-    let buffToken = new Buffer(token, 'base64');
-    let stringToken = buffToken.toString();
-    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
-
-    if (decodedToken) {
-        let query = `INSERT INTO "public"."presentations" ("presentationid","presentationownerid","data") VALUES(DEFAULT,'${decodedToken.id}','${data}') RETURNING "presentationid","presentationownerid"`;
-        response = await db.insert(query);
-        response=response[0];
-        res.status(200).json(response);
-    } else {
-        res.status(400).send("Wrong token");
-    }
+    let query = `INSERT INTO "public"."presentations" ("presentationid","presentationownerid","data") VALUES(DEFAULT,'${req.token.id}','${data}') RETURNING "presentationid","presentationownerid"`;
+    response = await db.insert(query);
+    response = response[0];
+    res.status(200).json(response);
     res.end();
 });
 
@@ -67,10 +41,11 @@ router.delete("/app/presentations", function (req, res) {
 
 });
 
-router.get("/application",function(req,res){
-    res.sendFile(__dirname+"/Application.html");
+router.get("/application", function (req, res) {
+    res.sendFile(__dirname + "/Application.html");
 })
-    
+
+
 
 
 module.exports = router;
--- js/users.js
+++ js/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require("./db.js");
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
+const authenticated = require("./authentication.js");
+
 const saltRounds = 10;
 
 const TOKEN_KEY = process.env.TOKEN_KEY;
@@ -20,17 +22,24 @@ router.get("/app/users", async function (req, res) {
 });
 */
 router.post("/app/user", async function (req, res) {
-    let email = req.body.email;
-    let username = req.body.name;
-    let password = req.body.password;
-    let userRole = 1;
-    await bcrypt.hash(password, saltRounds, async function (err, hash) {
-        if (email && username && hash) {
-            let query = `INSERT INTO "public"."users" ("username", "userid", "email", "role", "password") VALUES('${username}', DEFAULT, '${email}', '${userRole}', '${hash}')RETURNING "username", "email"`;
-            let code = await db.insert(query) ? 200 : 500;
-            res.status(code).end();
-        }
-    })
+    let auth = req.headers.authorization;
+    if (auth) {
+        let tmp = auth.split(' ');
+        let buf = new Buffer(tmp[1], 'base64');
+        let stringAuth = buf.toString();
+        let credentials = stringAuth.split(':');
+        let email = credentials[0].trim();
+        let username = credentials[1].trim()
+        let password = credentials[2].trim();
+        let userRole = 1;
+        await bcrypt.hash(password, saltRounds, async function (err, hash) {
+            if (email && username && hash) {
+                let query = `INSERT INTO "public"."users" ("username", "userid", "email", "role", "password") VALUES('${username}', DEFAULT, '${email}', '${userRole}', '${hash}')RETURNING "username", "email"`;
+                let code = await db.insert(query) ? 200 : 500;
+                res.status(code).end();
+            }
+        })
+    }
 });
 router.get("/app/user", async function (req, res) {
     let auth = req.headers.authorization;
@@ -45,10 +54,10 @@ router.get("/app/user", async function (req, res) {
         let query = `SELECT userid,username,password FROM public.users WHERE email='${email}'`;
 
         let user = await db.select(query);
-        
+
 
         if (user) {
-            user=user[0];
+            user = user[0];
             let correctPassword = await bcrypt.compare(password, user.password);
 
             if (correctPassword) {
@@ -78,61 +87,46 @@ router.get("/app/user", async function (req, res) {
         //Do something
     }
 });
-router.delete("/app/user", async function (req, res) {
-    let token = req.headers['x-access-token'];
-    let buffToken = new Buffer(token, 'base64');
-    let stringToken = buffToken.toString();
-    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
-    if (decodedToken) {
-        console.log(decodedToken);
-        let query=`DELETE FROM "public"."presentations" WHERE presentationownerid='${decodedToken.id}' RETURNING "presentationid"`
-        await db.delete(query);
-        query = `DELETE FROM "public"."users" WHERE userid='${decodedToken.id}' RETURNING "username", "email"`;
-        let response=await db.delete(query)
-        response=response[0];
-        res.status(200).json(response);
-    } else {
-        res.status(400).send("Wrong token");
-    }
+router.delete("/app/user", authenticated.valideAuthentication, async function (req, res) {
+    let query = `DELETE FROM "public"."presentations" WHERE presentationownerid='${req.token.id}' RETURNING "presentationid"`
+    await db.delete(query);
+    query = `DELETE FROM "public"."users" WHERE userid='${req.token.id}' RETURNING "username", "email"`;
+    let response = await db.delete(query)
+    response = response[0];
+    res.status(200).json(response);
+
     res.end();
 });
-router.put("/app/user", async function (req, res) {
-    console.log("yes!");
-    let token = req.headers['x-access-token'];
-    let buffToken = new Buffer(token, 'base64');
-    let stringToken = buffToken.toString();
-    let decodedToken = jwt.decode(stringToken, TOKEN_KEY);
-    if (decodedToken) {
-        let auth = req.headers.authorization;
-        let tmp = auth.split(' ');
-        let buf = new Buffer(tmp[1], 'base64');
-        let stringAuth = buf.toString();
-        let credentials = stringAuth.split(':');
-        let oldPassword = credentials[0].trim();
-        let newPassword = credentials[1].trim();
 
-        let query = `SELECT password FROM public.users WHERE userid='${decodedToken.id}'`;
+router.put("/app/user", authenticated.valideAuthentication, async function (req, res) {
+    let auth = req.headers.authorization;
+    let tmp = auth.split(' ');
+    let buf = new Buffer(tmp[1], 'base64');
+    let stringAuth = buf.toString();
+    let credentials = stringAuth.split(':');
+    let oldPassword = credentials[0].trim();
+    let newPassword = credentials[1].trim();
 
-        let hashPassword = await db.select(query);
-        hashPassword=hashPassword[0];
+    let query = `SELECT password FROM public.users WHERE userid='${req.token.id}'`;
 
-        if (hashPassword) {
-            let correctPassword = await bcrypt.compare(oldPassword, hashPassword.password);
-            if (correctPassword) {
-                await bcrypt.hash(newPassword, saltRounds, async function (err, hash) {
-                    query = `UPDATE public.users SET password='${hash}' WHERE userid='${decodedToken.id}' RETURNING "userid"`;
-                    let code = await db.update(query) ? 200 : 500;
-                    res.status(code).send("Password changed").end();
-                })
-            } else {
-                res.status(400).send("Wrong password").end();
-            }
+    let hashPassword = await db.select(query);
+    hashPassword = hashPassword[0];
+
+    if (hashPassword) {
+        let correctPassword = await bcrypt.compare(oldPassword, hashPassword.password);
+        if (correctPassword) {
+            await bcrypt.hash(newPassword, saltRounds, async function (err, hash) {
+                query = `UPDATE public.users SET password='${hash}' WHERE userid='${req.token.id}' RETURNING "userid"`;
+                let code = await db.update(query) ? 200 : 500;
+                res.status(code).send("Password changed").end();
+            })
         } else {
-            res.status(500).send("Problème with the server").end();;
+            res.status(400).send("Wrong password").end();
         }
     } else {
-        res.status(400).send("Wrong token").end();
+        res.status(500).send("Problem with the server").end();;
     }
+
 })
 
 module.exports = router;
